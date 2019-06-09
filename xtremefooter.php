2b8a11a2a06e7005925530454fc4c9582c167cf0
TheodoreM <teo.mixalis@hotmail.com>
2019-06-09 13:27:18 +0300

    Create xtremefooter.php
    c

---
+++ xtremefooter.php
@@ -0,0 +1,32 @@
+            <div id="content-bottom-border" class="container">
+            </div>
+            <div id="base">
+                <div class="container padding-vert-30 margin-top-60">
+                    <div class="row">
+                        <!-- Contact Details -->
+                        <div class="col-md-4 margin-bottom-20">
+                            <h3 class="margin-bottom-10">Contact Details</h3>
+                            <p>
+                                <span class="fa-phone">Telephone:</span> 6979518990
+                                <br>
+                                <span class="fa-envelope">Email:</span>
+                                <a href="mailto:info@joomla51.com">thomas_kaf13@hotmail.com</a>
+                                <br>
+                                <span class="fa-link">Website:</span>
+                                <a href="http://localhost/Xtreme/index.php">EXTREMESPORTS</a>
+                            </p>
+                            <p>Athens,
+                                <br>Greece</p>
+                        </div>
+                        <!-- End Contact Details -->
+
+                        <div class="col-md-8 margin-bottom-20">
+
+                            <h3 class="color-gray margin-bottom-10">Ο Καιρός</h3>
+							<!-- weather widget start -->
+							<a target="_blank" href="http://ibooked.gr/weather/piraeus-18334"><img src="https://w.bookcdn.com/weather/picture/3_18334_1_19_292c30_430_ffffff_333333_08488D_1_ffffff_333333_0_6.png?scode=124&domid=w209&anc_id=58940"  alt="booked.net"/></a>
+							<!-- weather widget end -->
+						</div>
+                    </div>
+                </div>
+            </div>
